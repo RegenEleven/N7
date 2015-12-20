db4e0742899114e2989f52e4de7e0002b79caf6f
Dan <dancrowley6@gmail.com>
2015-12-19 23:21:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 var version = "0.1";
 var startMessage = "Welcome Dan! You are currently using version " + version + "!";
-$[@id="active-song"]/div[1].replace("Active Song", "Your Music");
+function remove(id) {
+    return (elem=document.getElementById(id)).parentNode.removeChild(elem);
+}
+remove("static-announcement");
