6600db145ef46802e89655289304a24d8586054e
Dan <dancrowley6@gmail.com>
2015-12-20 13:40:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 var version = "0.1";
 var startMessage = "Welcome Dan! You are currently using version " + version + "!";
 $('#static-announcement').remove();
-document.getElementById("messages").style.height="calc(100%-34px)";
+document.getElementById("messages").style.height="calc(475px)";
