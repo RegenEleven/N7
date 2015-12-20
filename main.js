798df2f816b5340e3b251a15e86bd7d618ce6701
Dan <dancrowley6@gmail.com>
2015-12-19 23:43:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var version = "0.1";
 var startMessage = "Welcome Dan! You are currently using version " + version + "!";
 function remove(id) {
     return (elem=document.getElementById(id)).parentNode.removeChild(elem);
+    return (elem1=document.getElementById("messages").style.height="calc(100%-50px))";
 }
 remove("static-announcement");
-document.getElementById("messages").style.height="calc(100%-50px)";
