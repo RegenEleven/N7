1c7ea5b895371032b726ee668a8286c49e5412fa
Dan <dancrowley6@gmail.com>
2015-12-20 12:00:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ function remove(id) {
     return (elem=document.getElementById(id)).parentNode.removeChild(elem);
 }
 remove("static-announcement");
-document.getElementById("messages").style.height="470px";
+document.getElementById("messages").style.height="465px";
