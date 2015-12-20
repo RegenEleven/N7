c6bedd9030b4a87ccbc4966444270f9232d153d5
Dan <dancrowley6@gmail.com>
2015-12-19 23:07:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 var version = "0.1";
 var startMessage = "Welcome Dan! You are currently using version " + version + "!";
-$('p').append("<b>Appened Text</b>");
+$[@id="active-song"]/div[1].replace(Active Song, Your Music);
